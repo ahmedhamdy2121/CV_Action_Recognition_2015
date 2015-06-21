28025c1203a5f2fa5998f721ee6f0ddf37d8af42
ahmedhamdy90 <ahmed.johncena2010@gmail.com>
2015-06-21 15:14:37 +0200

    Minor Updates and gitignore fix
    I edited the gitignore file so I can synch the lib files.
    Also I made a minor updates in the listed files.

--- run.m
+++ run.m
@@ -1,7 +1,9 @@
-tic
+diary on
 for i = 1:1 % 1:3 (datasets)
+    tic
     for j = 1:1 % 1:5 (skeleton representation) 
         skeletal_action_classification(i,j)
     end
+    toc
 end
-toc
+diary off
